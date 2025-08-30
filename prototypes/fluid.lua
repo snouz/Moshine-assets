e0b4e99081bd6dab2c43c1da70f67d3b193f1a88
snouz <snooz-x@hotmail.com>
2025-08-30 23:35:28 +0200

    0.3.18

--- prototypes/fluid.lua
+++ prototypes/fluid.lua
@@ -1,14 +1,19 @@
 data:extend({
-{
-  type = "fluid",
-  name = "raw-data",
-  subgroup = "fluid",
-  default_temperature = 15,
-  base_color = {1, 1, 1},
-  flow_color = {1, 1, 1},
-  icon = "__Moshine-assets__/graphics/icons/raw-data.png",
-  order = "a[fluid]-a[water]-a[water]",
-  auto_barrel = false,
-},
-
+  {
+    type = "item-subgroup",
+    name = "data-fluid",
+    group = "fluids",
+    order = "b[data]"
+  },
+  {
+    type = "fluid",
+    name = "raw-data",
+    subgroup = "data-fluid",
+    order = "a[raw-data]",
+    default_temperature = 15,
+    base_color = {1, 1, 1},
+    flow_color = {1, 1, 1},
+    icon = "__Moshine-assets__/graphics/icons/raw-data.png",
+    auto_barrel = false,
+  },
 })
